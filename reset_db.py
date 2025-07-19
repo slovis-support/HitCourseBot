efa84cf6807bd1e9445b163ebd8af8b78b59d5e9
slovis-support <alexei.redichkin@yandex.ru>
2025-07-20 00:34:22 +0300

    Update reset_db.py

--- reset_db.py
+++ reset_db.py
@@ -1,6 +1,12 @@
-# reset_db.py
-from database import Base, engine
 
-# Удаление всех таблиц
-Base.metadata.drop_all(bind=engine)
-print("❌ Все таблицы удалены.")
+from database import Base, engine
+from sqlalchemy import inspect, text
+
+# Получаем соединение
+with engine.connect() as connection:
+    inspector = inspect(engine)
+    tables = inspector.get_table_names()
+
+    for table in tables:
+        connection.execute(text(f'DROP TABLE IF EXISTS "{table}" CASCADE'))
+        print(f"Удалена таблица: {table}")
