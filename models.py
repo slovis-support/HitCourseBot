0859dfa5fba64671b535fade4b170b03b3be9650
slovis-support <alexei.redichkin@yandex.ru>
2025-07-20 14:32:12 +0300

    Update models.py

--- models.py
+++ models.py
@@ -6,12 +6,17 @@ Base = declarative_base()
 
 class User(Base):
     __tablename__ = "users"
+    __table_args__ = {'extend_existing': True}  # ← добавлено!
+
     id = Column(Integer, primary_key=True, index=True)
     telegram_id = Column(String, unique=True, index=True)
     name = Column(String)
+    profile = Column(Text, nullable=True)
 
 class Message(Base):
     __tablename__ = "messages"
+    __table_args__ = {'extend_existing': True}  # ← добавлено!
+
     id = Column(Integer, primary_key=True, index=True)
     user_id = Column(String, index=True)
     role = Column(String)
